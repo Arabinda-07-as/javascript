ec32bf9995313253a4bcc277388f0dc3298da6be
Arabinda-07-as <samantaarabinda555@gmail.com>
2025-03-19 00:01:15 +0530

    Add files via upload

---
+++ basics/variables.js
@@ -0,0 +1,12 @@
+
+    const accId = 3335  /* We can't change the value of const keyword*/
+    let accName = "Arabinda"
+    var accPassword = "2134"
+
+    accName="Samanta" //It is allowed
+    accPassword="1234" //It is allowed
+    // accId=3337 //It is not allowed 
+
+    console.table([accId,accName,accPassword]);   
+
+    
