2f601b6dc16cf72072725bc4c347b5d9484befa8
Arabinda-07-as <samantaarabinda555@gmail.com>
2025-03-20 10:30:36 +0530

    Add files via upload

---
+++ basics/comparison.js
@@ -0,0 +1,18 @@
+
+// console.log("3" > 1);
+
+// console.log(null>0); // false
+// console.log(null==0); // false
+// console.log(null>=0); // true
+
+console.log(undefined>0); // false
+console.log(undefined==0); // false
+console.log(undefined>=0); // false
+
+console.log("4" === 4); // false 
+// (it check strictly, it checks the value and datatypes)
+
+// The difference between "==" and "==="
+// "==" checks the value only but the "===" checks the value and datatypes also
+
+
