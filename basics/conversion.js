ca1db4726adc946b6aeea8943ee783999da3c615
Arabinda-07-as <samantaarabinda555@gmail.com>
2025-03-20 10:30:09 +0530

    Add files via upload

---
+++ basics/conversion.js
@@ -0,0 +1,59 @@
+let score = "arabinda"
+console.log(typeof score);
+
+let valueinnumber = Number(score);
+console.log(typeof valueinnumber);
+console.log(valueinnumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1 || false => 0
+
+let isname = "arabinda"
+let inboolean = Boolean(isname);
+console.log(inboolean);
+console.log(typeof inboolean);
+
+// 1=> true; 0=>false
+// "arabinda"=> true
+//  ""=> false
+
+let scor = 33
+let instring = String(scor);
+
+console.log(typeof instring);
+console.log(instring);
+
+let isloggedin = 1
+let booleanisloggedin = Boolean(isloggedin)
+console.log(booleanisloggedin);
+
+//*********** Operations ************
+
+let value = 3
+let negvalue = -value
+console.log(negvalue);
+
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2/2);
+console.log(2**2); //power
+console.log(2%2);
+
+
+let str1 = "hello"
+let str2 = " hello"
+let str3 = str1+str2
+console.log(str3);
+
+console.log("1"+2); // 12
+console.log(1+"2"); // 12
+console.log("1"+2+2); // 122
+console.log(1+2+"2"); // 32
+
+console.log(+true); // 1
+console.log(+""); // 0
+
+
+
